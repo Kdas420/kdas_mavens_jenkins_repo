cb432df093a35fc768037544bc8979e57059333d
Kaushikdas <2021wb86308@wilp.bits-pilani.ac.in>
2025-07-13 15:52:06 +0530

    CI test: modified App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "CI Test: Hello Continuous Integration Trigger Test!" );
     }
 }
